7a25b2c3bab042384ff91d3984211232198d5039
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-05 23:45:41 -0400

    agregando imagen con sombra

---
+++ Fructus/View/FruitCardView.swift
@@ -0,0 +1,31 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Alejandro Hernández Lara on 05-08-21.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    // Properties
+    
+    // Body
+    var body: some View {
+        VStack(spacing: 20) {
+            Image("blueberry")
+                .resizable()
+                .scaledToFit()
+                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+            
+            Text("BlueBerry")
+        }
+    }
+}
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+//            .previewLayout(.fixed(width: 320, height: 640))
+    }
+}
