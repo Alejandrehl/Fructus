e5ab551705715bab319121c8ac09fbadb6d4f67f
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-05 20:28:22 -0400

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Alejandro Hernández Lara on 05-08-21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
