f6ab55ace2e6e3f924c4800620f8e67aff381a16
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-06 10:59:06 -0400

    Comenzando con agregado de boton

---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Alejandro Hernández Lara on 06-08-21.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+    }
+}
