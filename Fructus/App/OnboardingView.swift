7aa36f79fcd6f1a7cf332b5eaa43679a48084560
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-06 17:16:49 -0400

    Fix

--- Fructus/App/OnboardingView.swift
+++ Fructus/App/OnboardingView.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    var fruits: [Fruit] = fruitsData
+    
     var body: some View {
         TabView{
-            ForEach(0..<fruitsData.count) {item in FruitCardView(fruit: fruitsData[1])}
+            ForEach(fruits[0..<fruits.count]){
+                item in FruitCardView(fruit: item)
+            }
         }
         .tabViewStyle(PageTabViewStyle())
         .padding(.vertical, 20)
@@ -19,6 +23,6 @@ struct OnboardingView: View {
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(fruits: fruitsData)
     }
 }
--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -47,7 +47,6 @@ struct FruitCardView: View {
 
 struct FruitCardView_Previews: PreviewProvider {
     static var previews: some View {
-        FruitCardView(fruit: fruitsData[1])
-            .previewLayout(.fixed(width: 320, height: 640))
+        FruitCardView(fruit: fruitsData[0])
     }
 }
