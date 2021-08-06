90884f1d24e458aa4721de79e7536bf81de5efd1
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-06 10:57:05 -0400

    Más estilos

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -23,6 +23,12 @@ struct FruitCardView: View {
                     .foregroundColor(Color.white)
                     .font(.largeTitle)
                     .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                
+                Text("Blueberries are sweet, nutritious and widly popular fruit all over the world.")
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .frame(maxWidth: 480)
             }
         }
         .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: 0, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
