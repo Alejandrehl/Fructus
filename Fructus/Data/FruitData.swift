dc8e1c0e0bdb3df2cd4847ac58eb8015626955e2
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-06 16:33:06 -0400

    Comenzando con Array de frutas

--- Fructus/Data/FruitData.swift
+++ Fructus/Data/FruitData.swift
@@ -5,4 +5,8 @@
 //  Created by Alejandro Hernández Lara on 06-08-21.
 //
 
-import Foundation
+import SwiftUI
+
+let fruitsData: [Fruit] = [
+    Fruit(title: "Blueberry", headline: "Blueberries are sweat, nutritious and wildly popular fuit all over the world.", image: "blueberry", gradientColors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")], description: "Blueberries are perennial flowering plant with blue or purple berries.", nutrition: ["240 KJ", "9.96 g", '0.33 g', "0.74 g", "A", "B1", "Calcium", "Magnasium"])
+]
