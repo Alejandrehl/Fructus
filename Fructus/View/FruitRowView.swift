1e90cfc82c883987edf8b30855f28c60a65d4699
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-06 22:52:39 -0400

    utilizando v stack en h stack

--- Fructus/View/FruitRowView.swift
+++ Fructus/View/FruitRowView.swift
@@ -20,6 +20,12 @@ struct FruitRowView: View {
                 .shadow(color: Color(red: 0, green: 0, blue: 0), radius: 3, x: 2, y: 2)
                 .background(LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom))
                 .cornerRadius(8)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(fruit.title)
+                    .font(.title2)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            }
         }
     }
 }
