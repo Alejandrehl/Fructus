97ca4353c81918571067d7ff859fa4845fa821bd
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-07 12:18:26 -0400

    save

--- Fructus/App/FruitDetailView.swift
+++ Fructus/App/FruitDetailView.swift
@@ -11,7 +11,13 @@ struct FruitDetailView: View {
     var fruit: Fruit
     
     var body: some View {
-        Text(fruit.title)
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false){
+                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20) {
+                    Text(fruit.title)
+                }
+            }
+        }
     }
 }
 
