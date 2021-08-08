76eab797bbe662a80a07fda5bbb743b444e1474e
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-08 11:04:54 -0400

    Comenzando con fruit header

---
+++ Fructus/View/FruitHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  FruitHeaderView.swift
+//  Fructus
+//
+//  Created by Alejandro Hernández Lara on 08-08-21.
+//
+
+import SwiftUI
+
+struct FruitHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitHeaderView()
+            .previewLayout(.fixed(width: 375, height: 440))
+    }
+}
