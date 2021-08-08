9394f3c56f8cc235b2acafb8e39b4cae4023c7b5
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-07 23:04:44 -0400

    avances detalles

--- Fructus/App/FruitDetailView.swift
+++ Fructus/App/FruitDetailView.swift
@@ -22,6 +22,14 @@ struct FruitDetailView: View {
                             .fontWeight(.heavy)
                             .foregroundColor(fruit.gradientColors[1])
                         
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
                     }
                     .padding(.horizontal, 20)
                     .frame(maxWidth: 640, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
