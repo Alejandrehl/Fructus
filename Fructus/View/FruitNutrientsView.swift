89e5cf62032c99141aad8e76eea005e368628e8b
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-08 12:23:54 -0400

    Comenzando con fruits nutrients view

--- Fructus/View/FruitNutrientsView.swift
+++ Fructus/View/FruitNutrientsView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct FruitNutrientsView: View {
+    var fruit: Fruit
+    let nutrients: [String] = ["Energy", "Sugar", "Fat", "Protein", "Vitamins", "Minerals"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GroupBox() {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
 struct FruitNutrientsView_Previews: PreviewProvider {
     static var previews: some View {
-        FruitNutrientsView()
+        FruitNutrientsView(fruit: fruitsData[0])
+            .preferredColorScheme(.dark)
+            .previewLayout(.fixed(width: 375, height: 480))
+            .padding()
     }
 }
