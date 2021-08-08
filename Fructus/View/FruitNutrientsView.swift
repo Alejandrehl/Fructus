cdb6ffac88dc4d6c9cf900af294f54c9903e521c
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2021-08-08 12:23:47 -0400

    Comenzando con fruits nutrients view

---
+++ Fructus/View/FruitNutrientsView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitNutrientsView.swift
+//  Fructus
+//
+//  Created by Alejandro Hernández Lara on 08-08-21.
+//
+
+import SwiftUI
+
+struct FruitNutrientsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitNutrientsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitNutrientsView()
+    }
+}
